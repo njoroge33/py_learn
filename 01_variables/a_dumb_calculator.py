15d97c9330fd5df5944be45019b16361460e4740
Njoroge33 <johngichuhi769@gmail.com>
2019-04-14 13:03:15 -0700

    added functionality to sum user input

--- 01_variables/a_dumb_calculator.py
+++ 01_variables/a_dumb_calculator.py
@@ -4,3 +4,15 @@
 # Display the result 
 # Your program must support numbers with decimals and not just integers 
 # for more info on this quiz, go to this url: http://www.programmr.com/dumb-calculator-5
+
+
+def simple_calc():
+    numbers = input("Enter three numbers separated with a space: ")
+    lst = numbers.split()
+    total = 0
+    for i in lst:
+        total += int(i)
+    return total
+
+
+print(simple_calc())
