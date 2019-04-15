b9cf68201733ed97bc07ae138242a0e3351bec78
Njoroge33 <johngichuhi769@gmail.com>
2019-04-15 01:32:10 -0700

    added a function that ask a user for two numbers and mutltiplies them

--- 01_variables/multiply_two_numbers.py
+++ 01_variables/multiply_two_numbers.py
@@ -2,3 +2,16 @@
 # for more info on this quiz, go to this url: http://www.programmr.com/multiply-two-numbers-1
 
 
+def multiply_number():
+    print("Please enter two numbers")
+    user_inputs = []
+    result = 1
+    for i in range(2):
+        user_input = int(input("Enter a number: "))
+        user_inputs.append(user_input)
+    for i in user_inputs:
+        result = i * result
+    return result
+
+
+print(multiply_number())
