150f99902e8b14676fec41526000c26bc48e93ee
Njoroge33 <johngichuhi769@gmail.com>
2019-04-15 10:08:51 -0700

    added a function that swap the values  of two variables

--- 01_variables/swap_the_values_of_two_variable.py
+++ 01_variables/swap_the_values_of_two_variable.py
@@ -1,2 +1,15 @@
 # Write a program to swap the values of two variables 
 # for more info on this quiz, go to this url: http://www.programmr.com/swap-values-two-variable-1
+
+
+def swap_variables():
+    x = input("Enter value of X: ")
+    y = input("Enter value of Y: ")
+    a = x
+    x = y
+    y = a
+    print(f"After swapping, The value of X is, {x}, while value of Y is, {y},")
+    return x, y
+
+
+print(swap_variables())
