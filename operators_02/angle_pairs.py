e6870802f79929a205e499eeed8863457abf7e52
boswellgathu <boswell.gathu@andela.com>
2019-04-16 19:08:23 +0300

    add tests for operators/angle_pairs

---
+++ operators_02/angle_pairs.py
@@ -0,0 +1,7 @@
+# Complete the function named angle_pairs that accepts three angles (integers), measured in degrees, as parameters
+# It returns whether or not there exists both complementary and supplementary angles amongst the three angles passed
+# for more info on this quiz, go to this url: http://www.programmr.com/anglepairs
+
+
+def angle_pairs(a, b, c):
+    pass
