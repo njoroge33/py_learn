fb8aa3bddc04bb040f59fc52d5619357f389ebe9
njoroge33 <johngichuhi769@gmail.com>
2019-04-17 02:33:45 -0700

    added if __name__ ==__main__

--- operators_02/angle_pairs.py
+++ operators_02/angle_pairs.py
@@ -3,5 +3,12 @@
 # for more info on this quiz, go to this url: http://www.programmr.com/anglepairs
 
 
-def angle_pairs(a, b, c):
-    pass
+def angle_pairs(x, y, z):
+    if x + y == 90 or x + z == 90 or y + z == 90 or x + y == 180 or x + z == 180 or y + z == 180:
+        return True
+    else:
+        return False
+
+
+if __name__ == __main__:
+    angle_pairs(30, 40, 20)
