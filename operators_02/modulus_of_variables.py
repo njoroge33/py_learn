b3145396f46bee40a89e106da5e5a0cd0294c020
njoroge33 <johngichuhi769@gmail.com>
2019-04-17 22:21:20 -0700

    added a function that takes two numbers and returns their modulus

--- operators_02/modulus_of_variables.py
+++ operators_02/modulus_of_variables.py
@@ -1,5 +1,17 @@
-# Print the remainder of a/b. Make sure that b is not zero. If b is zero, display corresponding error message. Otherwise, display the result of remainder operation. 
+# Print the remainder of a/b. Make sure that b is not zero. If b is zero, display corresponding error message.
+# Otherwise, display the result of remainder operation.
 # Enter the 1st number:2
-Enter the 2nd number:4
-Remainder is: 2 
+# Enter the 2nd number:4
+# Remainder is: 2
 # for more info on this quiz, go to this url: http://www.programmr.com/modulus-variables-0
+
+
+def modulus(a, b):
+    if b == 0:
+        print("Math error")
+    else:
+        print(a % b)
+
+
+if __name__ == "__main__":
+    modulus(2, 4)
