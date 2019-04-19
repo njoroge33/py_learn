789332ae06ea00e1963018ae5340b6bd0915d7ee
boswellgathu <boswell.gathu@andela.com>
2019-04-19 23:32:18 +0300

    Update test_even_multiple_of_3.py

--- arithmetic_03/test/test_even_multiple_of_3.py
+++ arithmetic_03/test/test_even_multiple_of_3.py
@@ -4,9 +4,9 @@ from ..even_multiple_of_3 import even_multiple_of_3
 
 
 @pytest.mark.parametrize('number, expected', [
-    (300, True)
-    (240, True)
-    (15, False)
+    (300, True),
+    (240, True),
+    (15, False),
     (51, False)
 ])
 def test_even_multiple_of_3(number, expected):
