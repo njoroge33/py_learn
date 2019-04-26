830fe1bfa7ff0f3cf01d9eb81b5a70cd32e2138d
njoroge33 <johngichuhi769@gmail.com>
2019-04-25 17:25:57 -0700

    swapping pairs

--- lists_04/swappairs.py
+++ lists_04/swappairs.py
@@ -1,7 +1,20 @@
-# Write a method named swapPairs that accepts a list of strings as a parameter and switches the order of values in a pairwise fashion
-# Your method should switch the order of the first two values, then switch the order of the next two, switch the order of the next two, and so on
+# Write a method named swapPairs that accepts a list of strings as a parameter and
+#  switches the order of values in a pairwise fashion
+# Your method should switch the order of the first two values, then switch the order of the next two,
+#  switch the order of the next two, and so on
 # for more info on this quiz, go to this url: http://www.programmr.com/swappairs
 
 
 def swap_pairs(arr_x):
-    pass
+    b = []
+    c = []
+    for i in range(len(arr_x)-1):
+        b.append([arr_x[i], arr_x[i+1]])
+    print(b)
+    for i in reversed(b):
+        c.append(i)
+    print(c)
+
+
+if __name__ == "__main__":
+    swap_pairs([5, 7, 11, 4, 5])
