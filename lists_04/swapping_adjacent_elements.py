eb6710031b9ae623ce768a8370c3296054d5ba03
njoroge33 <johngichuhi769@gmail.com>
2019-04-26 08:17:39 -0700

    added a program that swaps adjacent pair

--- lists_04/swapping_adjacent_elements.py
+++ lists_04/swapping_adjacent_elements.py
@@ -1,3 +1,14 @@
 # Write a program to swap adjacent elements in an array 
 # Assume that array size is at least 1 
 # for more info on this quiz, go to this url: http://www.programmr.com/swapping-adjacent-elements
+
+
+def swap_adjacent(arr_x):
+    b = []
+    for i in range(len(arr_x)-1):
+        b.append((arr_x[i + 1], arr_x[i]))
+    print(list(b))
+
+
+if __name__ == "__main__":
+    swap_adjacent([1, 2, 3, 4])
