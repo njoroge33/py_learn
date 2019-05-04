623e6d57e4a21652ac2219ac9b80ce085613223a
njoroge33 <johngichuhi769@gmail.com>
2019-05-04 14:37:39 +0300

    added a function to check the number of adjacent elements which are equal

--- lists_04/count_clumps.py
+++ lists_04/count_clumps.py
@@ -2,5 +2,22 @@
 # Return the number of clumps in the given array
 # for more info on this quiz, go to this url: http://www.programmr.com/count-clumps-3
 
+
 def count_clumps(x_arr):
-    pass
+    count = 0
+    prev = ''
+    for i in range(len(x_arr)-1):
+        x = x_arr[i]
+        y = x_arr[i+1]
+        if x == y and not(x == prev):
+            count += 1
+            prev = x
+        elif y == prev:
+            prev = y
+        else:
+            prev = ''
+    return count
+
+
+if __name__ == "__main__":
+    print(count_clumps([1, 1, 1, 1, 1, 1]))
