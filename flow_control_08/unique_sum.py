f455c367f26ebab32f401fc6eaaed7c15d525978
njoroge33 <johngichuhi769@gmail.com>
2019-05-12 16:22:23 +0300

    created a test for unique sum

--- flow_control_08/unique_sum.py
+++ flow_control_08/unique_sum.py
@@ -2,3 +2,21 @@
 # However, if one of the values is the
 # same as another of the values, it does not count towards the sum
 # for more info on this quiz, go to this url: http://www.programmr.com/unique-sum
+
+
+def unique_sum(a, b, c):
+    original_lst = []
+    sec_lst = []
+    original_lst.append(a)
+    original_lst.append(b)
+    original_lst.append(c)
+    try:
+        for i in original_lst:
+            if i not in sec_lst:
+                sec_lst.append(i)
+        return sum(sec_lst)
+    except TypeError:
+        return "Wrong data type! Integers expected"
+
+
+print(unique_sum(3, 3, 3))
