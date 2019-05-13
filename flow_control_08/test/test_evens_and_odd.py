182c8174305123d9b70c8a239c94966656be2d17
njoroge33 <johngichuhi769@gmail.com>
2019-05-13 21:00:57 +0300

    repaired the tests

--- flow_control_08/test/test_evens_and_odd.py
+++ flow_control_08/test/test_evens_and_odd.py
@@ -10,15 +10,3 @@ from ..evens_and_odds import is_even_or_odd
 def test_is_even_or_odd(a, b, c, expected):
     actual = is_even_or_odd(a, b, c)
     assert actual == expected
-
-
-def test_is_even_or_odd_raises_error_value_error():
-    with pytest.raises(ValueError) as err:
-        is_even_or_odd('NOT A NUMBER')
-        assert err
-
-
-def test_is_even_or_odd_raises_error_zero_division_error():
-    with pytest.raises(ZeroDivisionError) as err:
-        is_even_or_odd(0)
-        assert err
