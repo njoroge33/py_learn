9458720ad7af8b6bda6806f2a1171b01d5930a02
njoroge33 <johngichuhi769@gmail.com>
2019-05-13 22:34:58 +0300

    changed the tests

--- flow_control_08/test/test_evens_and_odd.py
+++ flow_control_08/test/test_evens_and_odd.py
@@ -2,11 +2,11 @@ import pytest
 from ..evens_and_odds import is_even_or_odd
 
 
-@pytest.mark.parametrize('a, b, c, expected', [
+@pytest.mark.parametrize('a, expected', [
     (10, 'EVEN'),
     (51, 'ODD'),
     (0, 'EVEN')
 ])
-def test_is_even_or_odd(a, b, c, expected):
-    actual = is_even_or_odd(a, b, c)
+def test_is_even_or_odd(a, expected):
+    actual = is_even_or_odd(a)
     assert actual == expected
