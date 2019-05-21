023442159f3a7b238893b2a4ee702891bb42d32f
njoroge33 <johngichuhi769@gmail.com>
2019-05-21 15:46:25 +0300

    all exercises on functions

--- functions_10/sum_of_numbers_in_a_string.py
+++ functions_10/sum_of_numbers_in_a_string.py
@@ -3,6 +3,17 @@
 # for more info on this quiz, go to this url: http://www.programmr.com/sum-numbers-string
 
 
-
 def sum_numbers(x_str):
-	pass
+    y_str = []
+    lst = list("1234567890")
+    try:
+        for i in x_str:
+            if i in lst:
+                y_str.append(i)
+        return sum(map(int, y_str))
+    except TypeError:
+        return "Wrong data type! string expected"
+
+
+if __name__ == "__main__":
+    print(sum_numbers("k566am877"))
