b64c64730bb1deb9b6aad28ef002f6f1c0405e17
njoroge33 <johngichuhi769@gmail.com>
2019-05-22 14:54:55 +0300

    tests and exirces on methods

--- methods_11/multiply.py
+++ methods_11/multiply.py
@@ -2,4 +2,5 @@
 # for more info on this quiz, go to this url: http://www.programmr.com/multiply-0
 
 
-
+def multiply(num_1, num_2):
+    pass
--- methods_11/recursive_functions.py
+++ methods_11/recursive_functions.py
@@ -1,3 +1,7 @@
 # Write a recursive function to print the factorial of a number 
-# A recursive funtion is one which calls itself 
+# A recursive function is one which calls itself
 # for more info on this quiz, go to this url: http://www.programmr.com/recursive-functions
+
+
+def fact(num):
+    pass
