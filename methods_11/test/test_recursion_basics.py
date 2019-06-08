652df2e2fef4bba65dea094eb6629b701c486314
njoroge33 <johngichuhi769@gmail.com>
2019-06-08 12:35:36 +0300

    added test for generating fibonacci series

--- methods_11/test/test_recursion_basics.py
+++ methods_11/test/test_recursion_basics.py
@@ -1,10 +1,10 @@
 import pytest
-from ..recursion_basics import fibo
+from ..recursion_basics import gen_series
 
 
 @pytest.mark.parametrize("int_x, expected", [
     (10, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
 ])
 def test_recursion_basics(int_x, expected):
-    actual = fibo(int_x)
+    actual = gen_series(int_x)
     assert actual == expected
